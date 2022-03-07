e509f35c02df8ad025c7c8701d5dbadf98cd7f99
Juan Echavarria <juanea@gmail.com>
2022-03-07 15:58:42 +0000

    Updates 1:47:11

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import REACTLOGO from '../assets/react-logo.png';
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src={REACTLOGO} className="nav-logo" alt="React logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function MainContent() {
+  return (
+    <>
+      <h1>Reasons I&#39;m excited to learn React</h1>
+      <ol>
+        <li>
+          It is a popular library, I&#39;ll be able to fit in with the cool
+          kids!
+        </li>
+        <li>I&#39;m more likely to get a job as a developer if I know React</li>
+      </ol>
+    </>
+  );
+}
