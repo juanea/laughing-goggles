a56cfbd762470bc0ba3d10422d8dc7795f310bc6
Juan Echavarria <j.echavarria@ai-systems.ca>
2022-04-02 07:37:06 -0700

    Solo project header travel journal working

--- src/components/TravelJournal/Header.jsx
+++ src/components/TravelJournal/Header.jsx
@@ -4,13 +4,11 @@ import WORLDLOGO from '../../assets/TravelJournal/Fill213.png';
 
 export default function Header() {
   return (
-    <div className="header--background">
-      <ul className="header--list">
-        <li>
-          <img src={WORLDLOGO} className="header--logo" alt="World" />
-        </li>
-        <li className="header--title">my travel journal.</li>
-      </ul>
-    </div>
+    <header>
+      <div className="header--background">
+        <img src={WORLDLOGO} className="header--logo" alt="World" />
+        <p className="header--title">my travel journal.</p>
+      </div>
+    </header>
   );
 }
--- src/components/TravelJournal/data.js
+++ src/components/TravelJournal/data.js
@@ -1,3 +1,16 @@
 // https://images.unsplash.com/photo-1589308078059-be1415eab4c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNzMzOTgxMA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080
 // https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNzM0MDA3OQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080
 // https://images.unsplash.com/photo-1601439678777-b2b3c56fa627?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNzM0MDA3OQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080
+
+export default [
+  {
+    title: 'Mount Fuji',
+    location: 'Japan',
+    googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+    imageUrl: 'https://source.unsplash.com/WLxQvbMyfas',
+  },
+];
